dcf4300219b0b07f04d52a23301fca8aca4a246e
zdfowler <zfowler_uno@hotmail.com>
2015-07-14 18:24:15 +0000

    Added validation rules to fields

--- moviedb/app/models/movie.rb
+++ moviedb/app/models/movie.rb
@@ -1,8 +1,10 @@
 class Movie < ActiveRecord::Base
     
-    validates :title, presence: true
-    validates :year, presence: true
-    validates :rating, presence: true
-    validates :description, presence: true
+    validates :title, presence: true, length: {minimum: 2 }
+    validates :year, presence: true, numericality: { only_integers: true, greater_than: 1800, less_than: Date.today.year + 2.years }
+    validates :rating, presence: true, numericality: { only_integers: true, greater_than_or_equal_to: 0, less_than_or_equal_to; 10 }
+    validates :description, presence: true, length: {minimum: 5}
+    
+    
     
 end
